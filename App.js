28a1f614e5a86879bceef70c6d826abf874e7e79
Matt Polky <mdpolky+github@gmail.com>
2023-07-27 13:42:19 -0500

    added tab nav stubs

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MyTeamScreen from "./src/screens/MyTeamScreen";
+import NewsScreen from "./src/screens/NewsScreen";
+import ScoresScreen from "./src/screens/ScoresScreen";
+import StandingsScreen from "./src/screens/StandingsScreen";
+import MenuScreen from "./src/screens/MenuScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="SJS" component={MyTeamScreen} />
+        <Tab.Screen name="News" component={NewsScreen} />
+        <Tab.Screen name="Scores" component={ScoresScreen} />
+        <Tab.Screen name="Standings" component={StandingsScreen} />
+        <Tab.Screen name="Menu" component={MenuScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/screens/StandingsScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function StandingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Standings!</Text>
+    </View>
+  );
+}
