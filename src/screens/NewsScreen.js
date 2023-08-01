df86c3456d1426b7370427369ecfe9a149424503
Matt Polky <mdpolky+github@gmail.com>
2023-08-01 10:16:48 -0500

    add ref hook around webview to reload if it gets killed

--- src/screens/NewsScreen.js
+++ src/screens/NewsScreen.js
@@ -1,7 +1,14 @@
-import { Text, View } from "react-native";
+import { useRef } from "react";
 import { WebView } from "react-native-webview";
 
 //Unable to find any news-related api calls, so we'll just iframe it in
 export default function NewsScreen() {
-  return <WebView source={{ uri: "https://www.nhl.com/news/" }} />;
+  const inputRef = useRef(null);
+  return (
+    <WebView
+      source={{ uri: "https://www.nhl.com/news/" }}
+      ref={inputRef}
+      onContentProcessDidTerminate={inputRef.current?.reload}
+    />
+  );
 }
