a65aaa629f5aaa91699a9da952a55d7ff48e666f
Matt Polky <mdpolky+github@gmail.com>
2023-08-02 13:13:39 -0500

    updated playerThumb to check for image and use a placeholder if theres an issue

--- src/components/shared/common.js
+++ src/components/shared/common.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Image } from "expo-image";
 
 export const NhlTeamIcon = (props) => {
@@ -11,10 +12,23 @@ export const NhlTeamIcon = (props) => {
 };
 
 export const PlayerThumbnail = (props) => {
-  return (
-    <Image
-      {...props}
-      source={`https://cms.nhl.bamgrid.com/images/headshots/current/60x60/${props.id}.jpg`}
-    />
-  );
+  const imgPath = `https://cms.nhl.bamgrid.com/images/headshots/current/60x60/${props.id}.jpg`;
+  const [useDefault, setUseDefault] = useState(false);
+  const defaultImgPath =
+    "https://cms.nhl.bamgrid.com/images/headshots/current/60x60/skater.jpg";
+  useEffect(() => {
+    async function checkImage(request) {
+      try {
+        const response = await fetch(request);
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+      } catch (error) {
+        setUseDefault(true);
+      }
+    }
+    checkImage(imgPath);
+  }, []);
+
+  return <Image {...props} source={useDefault ? defaultImgPath : imgPath} />;
 };
